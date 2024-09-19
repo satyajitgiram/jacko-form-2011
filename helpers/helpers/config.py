1c0ed2896c315beb961fb451f36416f154106446
sagarmallik24 <107915084+sagarmallik24@users.noreply.github.com>
2024-09-19 12:32:45 +0530

    Delete helpers directory

--- helpers/helpers/config.py
+++
@@ -1,5 +0,0 @@
-
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SHEET_NAME = 'STUDENTS'
-SAMPLE_SPREADSHEET_ID='1KvNOLBRAhCBDo-cgZUsh4XnBZDAdfDC4MqqdqSwZedE'
-SERVICE_ACCOUNT_FILE = 'talmud-350506-bcde3688cfc5.json'
